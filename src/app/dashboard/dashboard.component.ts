b47f76ada3c2ea3942d822096d42202634efda77
gayatriganeshvisnagarkar-droid <gayatrivisnagarkar@gmail.com>
2026-06-03 13:28:07 +0530

    Added multi-page dashboard with tabs

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,32 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
 export class DashboardComponent {
 
-  selectedCategory = 'Users';
-  rangeValue = 50;
-  searchText = '';
+  currentTab = 'overview';
 
-  message = 'Users Data Selected';
-
-  updateDashboard() {
-
-    if (this.selectedCategory === 'Users') {
-      this.message = 'Users Data Selected';
-    }
-    else if (this.selectedCategory === 'Sales') {
-      this.message = 'Sales Data Selected';
-    }
-    else {
-      this.message = 'Orders Data Selected';
-    }
+  showTab(tab: string) {
+    this.currentTab = tab;
   }
 }
