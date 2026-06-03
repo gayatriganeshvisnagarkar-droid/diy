baa61e6909b474b55d5cf7cb74b9f402eb507ff6
gayatriganeshvisnagarkar-droid <gayatrivisnagarkar@gmail.com>
2026-06-03 13:16:21 +0530

    Added interactive dashboard components

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
 export class DashboardComponent {
+
+  selectedCategory = 'Users';
+
+  rangeValue = 50;
+
+  searchText = '';
+
+  categories = [
+    'Users',
+    'Sales',
+    'Orders'
+  ];
+
   users = [
     { id: 1, name: 'Gayatri', city: 'Nagpur' },
     { id: 2, name: 'Rahul', city: 'Pune' },
